78cf1306f78c451b7055f913392488e1bd1c8a46
Raj <rajbaidyanath76@gmail.com>
2026-04-08 00:18:18 +0530

    done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,20 +6,19 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin:[
+        "http://localhost:5173",
+        "https://resume-analyzer-pi-gold.vercel.app"
+    ],
+    credentials:true
 }))
 
-/* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
-/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
